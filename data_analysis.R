876eed92f56a1e6ede7faae6c57a4922c25c4622
Jiehua Chen <j@qed.ai>
2019-12-13 14:10:04 -0800

    modified data cleaning procedure.

--- data_analysis.R
+++ data_analysis.R
@@ -1,15 +1,19 @@
+library(dplyr)
+library(MESS)
+library(mgcv)
+library(grofit)
 
 # estimate generalized additive model (gam) from mgcv library
-
-PredictGrowthCurve <- function(data_property_unit, x, y, startx, endx){
-    data_fit <- data.frame(x=data_property_unit[, x], y = data_property_unit[, y])
-    fit <- gam(y~s(x), data=data_fit)
-    predictx <- seq(startx, endx, by=8)
-    predict_values <- data.frame(x=predictx, y= predict(fit, data.frame(x=predictx)))
+PredictGrowthCurve <- function(data, x, y, startx, endx) {
+    data_fit <- data.frame(x = data[, x], y = data[, y])
+    fit <- gam(y~s(x), data = data_fit)
+    predictx <- seq(startx, endx, by = 8)
+    predicty <- predict(fit, data.frame(x = predictx))
+    predict_values <- data.frame(x = predictx, y = predicty)
     return(predict_values)
 }
 
-GetPhenospexAUC <- function(planteye_data, property, mintime, maxtime){
+GetPhenospexAUC <- function(planteye_data, property, mintime, maxtime, spline) {
 
     dis_unit <- distinct(planteye_data, unit)
     n_dis_unit <- length(dis_unit)
@@ -19,40 +23,43 @@ GetPhenospexAUC <- function(planteye_data, property, mintime, maxtime){
     data_property <- planteye_data %>% 
             dplyr::select(timestamp, property, unit, genotype) %>% 
             mutate_(y = property)
-
     i <- 1
-
-    for (b in dis_unit$unit){
+    for (b in dis_unit$unit) {
+        print(b)
         data_property_unit <- data_property %>% filter(unit == b)
-        
         # align the prediction range for all the units
-        startx <- (mintime - as.numeric(data_property_unit$timestamp[1]))/3600
-        endx <- (maxtime - as.numeric(data_property_unit$timestamp[1]))/3600
-        data_property_unit$timestamp <- as.numeric(data_property_unit$timestamp - data_property_unit$timestamp[1])/3600
-        
-        predict_values <- PredictGrowthCurve(data_property_unit,
+        startx <- (mintime - as.numeric(data_property_unit$timestamp[1])) / 3600
+        endx <- (maxtime - as.numeric(data_property_unit$timestamp[1])) / 3600
+        data_property_unit$timestamp <- 
+          as.numeric(data_property_unit$timestamp - data_property_unit$timestamp[1]) / 3600
+        # estimate smooth spline
+        if(spline) {
+            predict_values <- PredictGrowthCurve(data_property_unit,
                                         "timestamp",
                                         property,
                                         startx,
                                         endx)
-        
-        lentime <- (maxtime - mintime)/3600
-        auc_temp[i] <- auc(predict_values$x, predict_values$y) - lentime*predict_values$y[1] 
+        }
+        else{
+          # estimate parametric growth curve models
+            result <- gcFitModel(data_property_unit$timestamp, data_property_unit$Height)
+        }
+        lentime <- (maxtime - mintime) / 3600
+        auc_temp[i] <- auc(predict_values$x, predict_values$y) - lentime * predict_values$y[1] 
         genotype[i] <- as.character(data_property_unit$genotype[1])
         i <- i + 1
-    
     }
-    auc_data <- data.frame(unit=dis_unit, auc = auc_temp, genotype)
+    auc_data <- data.frame(unit = dis_unit, auc = auc_temp, genotype)
+    auc_data <- filter(auc_data, auc > 0)
     return(auc_data)
-    
-    }
+}
 
-PhenospexANOVA <- function(auc_data, properties){
+PhenospexANOVA <- function(auc_data, properties) {
     n_dis_prop <- length(properties)
     mse_results <- rep(NA, n_dis_prop)
     pvalues_results <- rep(NA, n_dis_prop)
     k <- 1
-    for (property in properties){
+    for (property in properties) {
         aov_genotype <- aov(auc_data[, property] ~ auc_data$genotype)
         aov_results <- summary(aov_genotype)[[1]]
         mse_results[k] <- aov_results[2, 3]
@@ -64,5 +71,4 @@ PhenospexANOVA <- function(auc_data, properties){
                          mse_results = mse_results,
                          pvalues_results = pvalues_results),
               "auc_anova_results.csv", row.names = FALSE)
-}
-
+    }
--- data_cleaning.R
+++ data_cleaning.R
@@ -1,19 +1,11 @@
-
-FitGrowthCurve <- function(data, x, y){
-    fit <- gam(data[,y]~s(data[, x]))
-    fitted_values <- data.frame(x = data[,x], y = fit$fitted.values)
-    r2value <- summary(fit)$r.sq
-    return(list(fitted_values = fitted_values, rsq = r2value))
+# R square function
+rsquare <- function(x, y) {
+    rsquare <- 1 - sum((x - y)^2) / sum((y - mean(y))^2)
+    return(rsquare)
 }
 
-# keep only data with good spline fitting (R2>0.9)
-
-data_cleaning <- function(planteye_data, property){
-
+data_cleaning <- function(planteye_data, property) {
     dis_unit <- distinct(planteye_data, unit)
-    n_dis_unit <- length(dis_unit)
-
-
     data_property <- planteye_data %>%
     dplyr::select(timestamp, property, unit, genotype) %>%
     mutate_(y = property)
@@ -21,41 +13,31 @@ data_cleaning <- function(planteye_data, property){
     fitted_values_total <- rep(NA, 3)
     error_unit <- NA
     planteye_data_cleaned <- NULL
-    n <- 0
-    for (b in dis_unit$unit){
-        print(b)
+    for (b in dis_unit$unit) {
+        print(b) 
         data_property_unit <- data_property %>% filter(unit == b)
-        #
-        # outliers <-  dbscanOutlierDetect(data_property_unit, 60, log(nrow(data_property_unit)), FALSE)
-        # table_outliers <- table(outliers)
-        # n_max_cluster <- max(table_outliers)
-        # max_cluster <- as.numeric(names(table_outliers))[table_outliers == n_max_cluster]
-        #
-        # if(n_max_cluster<dim(data_property_unit)[1]*3/4){
-        #     error_unit <- c(error_unit, b)
-        # }
-        # else{
-        #     n <- n+n_max_cluster
-        #     data_property_unit_cleaned <- data_property_unit[outliers==max_cluster, ]
-        #     planteye_data_cleaned <- rbind(planteye_data_cleaned, data_property_unit_cleaned)
-        # }
-        #
-        data_property_unit$timestamp <- as.numeric(data_property_unit$timestamp - data_property_unit$timestamp[1])/3600
-        fitted_values <- FitGrowthCurve(data_property_unit,
-                                        "timestamp",
-                                        property)
-
-        rsq <- fitted_values$rsq
-        if(rsq< 0.9){error_unit <- c(error_unit, b)}
-        
-    
+        result_lm <- lm(Height~ timestamp, data = data_property_unit)
+        resid_lm <- residuals(result_lm)
+        median_resid <- (abs(resid_lm) - median(resid_lm)) / mad(resid_lm)
+        # only keep points with standardized mad less than 2.5
+        data_property_unit <- data_property_unit[median_resid < 2.5, ]
+        # keep unit with more than 30 data points
+        if (dim(data_property_unit)[1] > 30) {
+            result_grofit <- gcFitModel(data_property_unit$timestamp, data_property_unit$Height)
+            if (! result_grofit$fitFlag) {
+                error_unit <- c(error_unit, b)
+            }
+            # keep units with larger than 0.3 R2 of growth model fitting 
+            else if (rsquare(result_grofit$fit.data, result_grofit$raw.data)<0.3) {
+                error_unit <- c(error_unit, b)                 
+            } else {
+                planteye_data_cleaned<-rbind(planteye_data_cleaned, data_property_unit) 
+            }
+        }
+        else {
+            error_unit <- c(error_unit, b) 
+        }
     }
-
-
     error_unit <- error_unit[-1]
-    planteye_data_cleaned <- filter(planteye_data, !(unit %in% error_unit))
-
     return(planteye_data_cleaned)
-
 }
-
